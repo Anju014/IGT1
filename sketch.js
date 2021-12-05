a6b33090f7091045164276b517130c8e4ba711bc
Anju014 <84642983+Anju014@users.noreply.github.com>
2021-12-05 19:34:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// TASKS
+// Step 1: Spawn the enemies (enemy1 and enemy2 should come from right side whereas enemy3 and enemy4 should come from left side)
+// Step 2: Create groups for birds and enemies and add sprites to it.
+// Step 3: Assign Game States - play and end
+// Step 4: if boy touches enemies, gamestate changes to end. Add hit sound.
+
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(600,400)
+  
+}
+
+function draw(){
+  background("white");
+  drawSprites();
+}
